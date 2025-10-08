0b5704bf7167d9f2308eeeda3019a5a26793eda4
Md Salauddin <mdsalauddinahmed.amir@gmail.com>
2025-10-08 10:45:30 +0600

    cloudinary

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express"
 import cors from "cors"
+import cookieParser from "cookie-parser"
 // import cookieParser from "cookie-parser"
 
 export const app = express()
@@ -9,3 +10,9 @@ app.use(cors({
     origin:process.env.cors_origin,
     Credentials:true
 }))
+app.use(express.json({limit:"16kb"}))
+app.use(express.urlencoded({extended:true,limit:"16kb"}))
+app.use(express.static("public"))
+app.use(cookieParser())
+
+export {app}
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,26 @@
+import { v2 as cloudinary} from 'cloudinary';
+import { response } from 'express';
+import fs from 'fs'
+import { loadEnvFile } from 'process';
+
+cloudinary.config({ 
+  cloud_name:process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key:process.env.CLOUDINARY_API_KEY, 
+  api_secret:process.env.CLOUDINARY_API_SECRET
+});
+
+const uploadOnCloudinary = async (localFilePath)=>{
+    try{
+        if(!localFilePath) return null
+        cloudinary.uploader.upload(localFilePath,{
+            resource_type:"auto"
+        })
+        console.log("File is uploaded on cloudinary",response.url);
+        return response
+    }catch(err){
+        fs.unlinkSync(localFilePath)
+        return null
+    }
+}
+
+export {uploadOnCloudinary}
